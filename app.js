b75a7f2d647b614975b699bf693d216ac1250ec0
duckheewon <fain9301@yahoo.com>
2018-02-06 12:49:26 +0900

    add seession

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var passport = require('passport');
+var bcrypt = require('bcrypt-nodejs');
+var flash = require('connect-flash');
 var index = require('./server/routes/index');
 
 
@@ -20,6 +23,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+    secret: 'secretkeywon',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 // 쿠키 유효기간 1시간
+    }
+}));
 //get public folder url (css, javascript, bootstrap)
 app.use('/static', express.static(path.join(__dirname, 'public')));
 //get camera image url
