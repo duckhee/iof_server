6039b9a6422780a9b1037b001c9897004f3ff920
duckheewon <fain9301@yahoo.com>
2018-02-06 12:38:22 +0900

    file add

--- app.js
+++ app.js
@@ -20,27 +20,35 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//get public folder url (css, javascript, bootstrap)
+app.use('/static', express.static(path.join(__dirname, 'public')));
+//get camera image url
+app.use('/images', express.static(path.join(__dirname, 'camera_images')));
+//get upload file url
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+//zip file download
+app.use('/download', express.static(path.join(__dirname, 'download')));
+//index router
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
