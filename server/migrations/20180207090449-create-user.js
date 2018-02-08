175f77648429c4bc53ff5c9bc6586c7caf784cbc
duckheewon <fain9301@yahoo.com>
2018-02-08 16:15:05 +0900

    model type add

--- server/migrations/20180207090449-create-user.js
+++ server/migrations/20180207090449-create-user.js
@@ -1,53 +1,54 @@
 'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id: {
-        type: Sequelize.STRING,
-        unique:true
-      },
-      user_password: {
-        type: Sequelize.STRING
-      },
-      user_name: {
-        type: Sequelize.STRING
-      },
-      user_phone1: {
-        type: Sequelize.INTEGER
-      },
-      user_phone2: {
-        type: Sequelize.INTEGER
-      },
-      user_phone3: {
-        type: Sequelize.INTEGER
-      },
-      user_address1: {
-        type: Sequelize.STRING
-      },
-      user_address2: {
-        type: Sequelize.STRING
-      },
-      apikey:{
-        type:DataTypes.STRING,
-        unique:true
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('users');
-  }
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            user_id: {
+                type: Sequelize.STRING,
+                unique: true
+            },
+            user_password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            user_name: {
+                type: Sequelize.STRING
+            },
+            user_phone1: {
+                type: Sequelize.INTEGER
+            },
+            user_phone2: {
+                type: Sequelize.INTEGER
+            },
+            user_phone3: {
+                type: Sequelize.INTEGER
+            },
+            user_address1: {
+                type: Sequelize.STRING
+            },
+            user_address2: {
+                type: Sequelize.STRING
+            },
+            apikey: {
+                type: DataTypes.STRING,
+                unique: true
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('users');
+    }
 };
