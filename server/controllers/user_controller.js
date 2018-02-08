62e524db4b5dd2a9d252e64eba19ee04f4099c5b
duckheewon <fain9301@yahoo.com>
2018-02-08 16:32:06 +0900

    user controller

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,19 +1,35 @@
 var models = require('../models/index');
 var user = require('../models/user');
 
-exports.create = function(user_info, callback)
-{
-    models.user.findOrCreate({
-        where:{
 
-        }, 
-        default:
-        {
+exports.create = function(user_info, callback) {
+    models.user.findOrCreate({
+        where: {
+
+        },
+        default: {
 
         }
-    }).spread().catch(function(err)
-{
-
-});
+    }).spread(function(user, created) {
+        if (created) {
+            callback(null, null, created);
+        } else {
+            callback(null, user.dataValues, null);
+        }
+    }).catch(function(err) {
+        callback(err, null, null);
+    });
 };
 
+//check id
+exports.check_id = function(user_info, callback) {
+    models.user.findAndCount({
+        where: {
+            user_id: user_info.user_id
+        }
+    }).then(function(row) {
+        callback(null, row);
+    }).catch(function(err) {
+        callback(err, null);
+    });
+};
