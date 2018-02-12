2ac512ca484eafbaee50ebedb6aa8f958f52f8e1
duckhee <fain9301@yahoo.com>
2018-02-12 09:52:12 +0900

    win32 gangNam
    win32 gangNam

--- server/routes/users/user.js
+++ server/routes/users/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt-nodejs');
-
+var custom_util = require('../../util/util'); //create apikey
 var user_controller = require('../../controllers/user/user_controller');
 
 //registe user page
@@ -11,7 +11,29 @@ router.get('/registe', function(req, res, next) {
 });
 //registe user page post process
 router.post('/process/registe', function(req, res, next) {
-    next();
+    consoel.log('registe post router');
+    
+    var userid = req.body.id || req.query.id || req.param.id || req.params.id;
+    var userpw = req.body.pw || req.query.pw || req.param.pw || req.params.pw;
+    var username = req.body.name || req.query.name || req.param.name || req.params.name;
+    var userphone1 = req.body.phone1 || req.query.phone1 || req.param.phone1 || req.params.phone1;
+    var userphone2 = req.body.phone2 || req.query.phone2 || req.param.phone2 || req.params.phone2;
+    var userphone3 = req.body.phone3 || req.query.phone3 || req.param.phone3 || req.params.phone3;
+    var useraddress1 = req.body.address1 || req.query.address1 || req.param.address1 || req.params.address1;
+    var useraddress2 = req.body.address2 || req.query.address2 || req.param.address2 || req.params.address2;
+    
+    var user_info = {
+        user_id:userid,
+        user_pw:userpw, 
+        user_name:username,
+        user_phone1:userphone1,
+        user_phone2:userphone2,
+        user_phone3:userphone3,
+        user_address1:useraddress1,
+        user_address2:useraddress2
+    };
+
+
 });
 
 //router ajax user id check page
@@ -26,7 +48,8 @@ router.get('/login', function(req, res, next) {
 
 //router login post process
 router.post('/process/login', function(req, res, next) {
-    next();
+    console.log('login post router');
+    
 });
 
 //router profile page
