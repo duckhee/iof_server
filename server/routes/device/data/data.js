3f8be24a9c9e1c91a7f9c1f678b067bb02bcd53f
duckhee <fain9301@yahoo.com>
2018-03-11 16:31:18 +0900

    testing insert data

--- server/routes/device/data/data.js
+++ server/routes/device/data/data.js
@@ -59,35 +59,45 @@ router.get('/insert', function(req, res, next) {
 
 //router insert data
 router.post('/insert', function(req, res, next) {
-    var query_apikey = req.query.apikey || req.params.apikey || req.body.apikey || req.param.apikey;
-    var data_value = req.body.value || req.params.value || req.query.value || req.param.value;
-    var apikey_info = { apikey: query_apikey };
-    var insert_data = {};
-    device_controller.check_device(apikey_info, function(err, result) {
-        if (err) {
-            console.log('check device error :::::', err);
-            next(err);
-        } else {
-            console.log('device search id ::::', result);
+    var query_apikey = req.query.serial || req.params.serial || req.body.serial || req.param.serial;
+    var insert_data = req.query.value || req.params.value || req.body.value || req.param.value;
+    var apikey_info = {
+        apikey: query_apikey,
+    };
 
-            //data insert info
-            insert_data = {
-                deviceId: result,
+    device_controller.insert_before(apikey_info, function(err, row) {
+        if (err) {
+            console.log('insert before data error ::: ', err);
+            res.json('failed');
+        } else if (row) {
+            console.log('inset before data success :::::', row.id);
+            var data_info = {
+                data: insert_data,
                 apikey: query_apikey,
-                value: data_value
-            };
-            data_controller.insert_value(insert_data, function(err, row) {
+                device_id: row.id,
+                serial: query_apikey,
+                sd_address: row.address
+            }
+            data_controller.insert_value(data_info, function(err, row) {
                 if (err) {
-                    console.log('insert data error :::::: ', err);
-                    next(err);
+                    res.status(404);
                 } else {
-                    console.log('insert success ::::::', row);
-                    res.json('success'); //example send json 
+                    network_controller.update_actstatus(data_info, function(err, result) {
+                        if (err) {
+                            console.log('updateing active network error ::::: ', err);
+                            res.status(404);
+                        } else {
+                            console.log('success updating network set ::::: ', result);
+                            res.json('success');
+                        }
+                    })
+
                 }
             });
+        } else {
+            res.status(500);
         }
     });
-
 });
 
 //router ajax get data json
