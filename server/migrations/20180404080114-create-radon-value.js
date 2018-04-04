79aa154faea91fd456ec2c4a36134febf9300290
duckheewon <fain9301@yahoo.com>
2018-04-04 17:12:54 +0900

    testing db

---
+++ server/migrations/20180404080114-create-radon-value.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('radon_values', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            rd_value: {
+                type: Sequelize.STRINNG
+            },
+            deviceId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'devices',
+                    key: 'id'
+                },
+                onDelete: 'CASCADE',
+            },
+            td_data: {
+                type: Sequelize.TEXT
+            },
+            rd_serial: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: 'TIMESTAMP',
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: 'TIMESTAMP',
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('radon_values');
+    }
+};
