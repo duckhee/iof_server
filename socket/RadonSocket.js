5062e25f1159626562c3da7c71e383348d67c62d
duckhee <fain9301@yahoo.com>
2018-04-25 18:18:51 +0900

    radon socket add

---
+++ socket/RadonSocket.js
@@ -0,0 +1,6 @@
+var DeviceController = require('../server/controllers/device/device_controller');
+var RadonController = require('../server/controllers/device/radon_controller');
+
+module.exports = function(io, socket) {
+
+}
