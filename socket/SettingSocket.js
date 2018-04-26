239d5ee682b7d97340b36def35e62b2c7273f0c5
duckhee <fain9301@yahoo.com>
2018-04-26 16:21:23 +0900

    testing setting

--- socket/SettingSocket.js
+++ socket/SettingSocket.js
@@ -8,6 +8,55 @@ var Util = require('../server/util/util');
 
 //socket start
 module.exports = function(io, socket) {
+    //set device setting 
+    socket.on('web_device_setting_request', function(data) {
+            console.log('web setting device', data);
+            var DeviceChecking = {
+                "serial": data.serial
+            }
+            DeviceController.insert_before(data, function(result) {
+                if (err) {
+                    console.log('checking device error before setting :::::', err);
+                } else if (result) {
+                    console.log('find device before setting ::::', result);
+                    if (!Util.isEmpty(result)) {
+                        if (result.device_type === 'IoF') {
+                            var IoFSettingValue = {
+                                "deviceId": result.id,
+                                "sd_serial": result.device_serial,
+                                "sd_address": data.address,
+                                "water_time": data.waterTime,
+                                "camera_time": data.cameraTime,
+                                "sensing_time": data.sensingTime
+                            }
+                            IoFSettingController.InsertSetting(IoFSettingValue, function(err, result) {
+                                if (err) {
+                                    console.log('insert iof setting socket error ::: ', err);
+                                } else if (result) {
+                                    var SendSetting = {
+                                        "waterTime": data.waterTime,
+                                        "cameraTime": data.cameraTime,
+                                        "sensingTime": data.sensingTime
+                                    }
+                                    io.emit('send_device_setting_web_' + result.device_serial, SendSetting);
+                                } else {
+                                    console.log('insert setting null');
+                                }
+                            });
+                        } else if (result.device_type === 'radon') {
+                            var RadonSetting = {
+
+                            }
+                        } else {
+                            console.log('can not found device type');
+                        }
+                    }
+                    //checking null end
+                }
+            });
+            //insert before end
+        })
+        //checking device setting
     socket.on('device_setting_request', function(data) {
         console.log('device setting before check device :::', data);
         var deviceInfo = {};
@@ -17,9 +66,11 @@ module.exports = function(io, socket) {
             } else if (result) {
                 if (!Util.isEmpty(result)) {
                     if (result.device_type === 'IoF') {
+                        //iof setting how to ??
+
 
                     } else if (result.device_type === 'radon') {
-
+                        //radon setting how to ??
                     }
                 } else {
 
