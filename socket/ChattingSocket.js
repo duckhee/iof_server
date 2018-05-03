ed195d387924101c5515a6a0c7f4a175f02f4ff6
duckhee <fain9301@yahoo.com>
2018-05-03 14:29:30 +0900

    tesitng

--- socket/ChattingSocket.js
+++ socket/ChattingSocket.js
@@ -1,4 +1,5 @@
 var socketio = require('socket.io');
+var socketclient = require('socket.io-client')('https://13.209.19.28:5001');
 
 module.exports = function(server) {
     var io = socketio.listen(server);
