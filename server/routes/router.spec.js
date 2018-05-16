a7d45d9cb07e5ad78fe63759180a0d97a1e71d57
duckhee <fain9301@yahoo.com>
2018-05-16 17:49:30 +0900

    testing

---
+++ server/routes/router.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const should = require('should');
+const mocha = require('mocha');
+
+describe('GET /', () => {
+    it('return /', () => {
+        //assert.equal(1, 1);
+        (1).should.equal(1);
+    });
+});
